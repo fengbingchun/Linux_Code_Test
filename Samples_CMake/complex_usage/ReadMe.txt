ef9a789e65830867a55022822078e5d7b793a8b8
fengbingchun <fengbingchun@163.com>
2018-08-15 08:45:30 +0800

    Fix Samples_CMake/complex_usage

--- Samples_CMake/complex_usage/ReadMe.txt
+++ Samples_CMake/complex_usage/ReadMe.txt
@@ -1,5 +1,5 @@
 Execute in turn:
-	$ bash ./make_api.sh --no-test
-	$ bash ./make_api.sh
+	$ ./build.sh --no-test
+	$ ./build.sh
 	export LD_LIBRARY_PATH=api_install/lib
 	$ ./api_install/bin/test
--- Samples_CMake/complex_usage/build.sh
+++ Samples_CMake/complex_usage/build.sh
@@ -1,6 +1,6 @@
-﻿#!/bin/bash 
+﻿#! /bin/bash 
 
-#set -e #Exit immediately if a command exits with a non-zero status
+# set -e # Exit immediately if a command exits with a non-zero status
 
 PROJ_ROOT=$PWD
 BUILD_ROOT=$PROJ_ROOT
@@ -14,13 +14,15 @@ fi
 
 echo "no test: $NO_TEST"
 
-#mkdir的-p选项允许一次创建多层次的目录，而不是一次只创建单独的目录
+# mkdir的-p选项允许一次创建多层次的目录，而不是一次只创建单独的目录
 mkdir -p $BUILD_ROOT/build
 cd $BUILD_ROOT/build
 cmake -DCMAKE_CXX_FLAGS=-g -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX:PATH=$BUILD_ROOT/install $PROJ_ROOT
-make -j4 #并行处理,cpu个数
-make install > /home/spring/math_oper1.txt
+make -j4 # 并行处理,cpu个数
+make install #> ${BUILD_ROOT}/build/math_oper1.txt
+
+cd -
 
 mkdir -p $BUILD_ROOT/api/build
 cd $BUILD_ROOT/api/build
@@ -28,4 +30,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DNO_TESTBENCH=$NO_TEST \
 	-DSDK_INSTALL_PATH=$BUILD_ROOT/install \
 	-DCMAKE_INSTALL_PREFIX:PATH=$PROJ_ROOT/api_install $PROJ_ROOT/api
 make -j4
-make install > /home/spring/math_oper2.txt
+make install #> ${BUILD_ROOT}/build/math_oper2.txt
+
+cd -
